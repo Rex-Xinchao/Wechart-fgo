d53fd009739d44cf8f3168e6d0245628684d1430
Rex Sun <rex.sun@chinascope.com>
2020-04-07 15:27:40 +0800

    index完善

---
+++ data/charactor.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+        "id": "1",
+        "name": "亚瑟",
+        "classify": "Saber",
+        "star": "5",
+        "imageUrl": "/static/images/leftRight.png"
+    },
+    {
+        "id": "2",
+        "name": "卫宫士郎",
+        "classify": "Archer",
+        "star": "4",
+        "imageUrl": "/static/images/leftRight.png"
+    },
+    {
+        "id": "3",
+        "name": "库丘林",
+        "classify": "Lancer",
+        "star": "3",
+        "imageUrl": "/static/images/leftRight.png"
+    },
+    {
+        "id": "4",
+        "name": "亚历山大",
+        "classify": "Rider",
+        "star": "5",
+        "imageUrl": "/static/images/leftRight.png"
+    },
+    {
+        "id": "5",
+        "name": "吉尔",
+        "classify": "Caster",
+        "star": "3",
+        "imageUrl": "/static/images/leftRight.png"
+    },
+    {
+        "id": "6",
+        "name": "哈桑",
+        "classify": "Assassin",
+        "star": "3",
+        "imageUrl": "/static/images/leftRight.png"
+    },
+    {
+        "id": "7",
+        "name": "海格力斯",
+        "classify": "Basaker",
+        "star": "4",
+        "imageUrl": "/static/images/leftRight.png"
+    }
+]
+
+module.exports = data
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,46 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const DataList = require('../../data/charactor.js')
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
+    dataList: [],
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    dialogShow: false,
+    typeList: [{
+      name: 'Saber',
+      checked: false
+    }, {
+      name: 'Archer',
+      checked: false
+    }, {
+      name: 'Lancer',
+      checked: false
+    }, {
+      name: 'Rider',
+      checked: false
+    }, {
+      name: 'Caster',
+      checked: false
+    }, {
+      name: 'Assassin',
+      checked: false
+    }, {
+      name: 'Basaker',
+      checked: false
+    }],
+    tagList: ['Saber', 'Archer', 'Rider'],
+    checkedList: []
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -30,6 +49,7 @@ Page({
           hasUserInfo: true
         })
       }
+      this.getDataList();
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
@@ -43,12 +63,33 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  getDataList: function() {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      dataList: DataList
+    })
+  },
+  showDialog: function() {
+    let result = this.data.typeList.map(item => {
+      item.checked = this.data.tagList.find(i => i === item.name)
+      return item
+    })
+    console.log(result)
+    this.setData({
+      dialogShow: true,
+      typeList: result
+    })
+  },
+  closeDialog: function(e) {
+    let result = this.data.checkedList.slice(0)
+    this.setData({
+      dialogShow: false,
+      tagList: result,
+      checkedList: []
+    })
+  },
+  checkboxChange: function(e) {
+    this.setData({
+      checkedList: e.detail.value
     })
   }
 })
